7bea33e97d259e4b0e7fcbbbc19ae7e558999759
Dwayne Crooks <me@dwaynecrooks.com>
2019-08-25 09:52:50 -0400

    LETREC: Start with PROC

---
+++ 05-ch3/interpreters/LETREC/src/Letrec/Env.hs
@@ -0,0 +1,18 @@
+module Letrec.Env (Env, empty, extend, apply) where
+
+data Env k v = Env [(k, v)] deriving Show
+
+empty :: Env k v
+empty = Env []
+
+extend :: k -> v -> Env k v -> Env k v
+extend k v (Env bs) = Env ((k, v) : bs)
+
+apply :: (Eq k, Show k) => Env k v -> k -> v
+apply (Env bs) k =
+  case lookup k bs of
+    Nothing ->
+      error ("No binding for " ++ show k)
+
+    Just v ->
+      v
