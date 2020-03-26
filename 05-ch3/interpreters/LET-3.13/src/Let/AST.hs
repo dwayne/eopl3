a870ab0211c9067dce87deedb271bd9577235cfe
Dwayne Crooks <me@dwaynecrooks.com>
2020-03-25 20:17:43 -0400

    ex3.13 - Start with exercise 3.8's interpreter

---
+++ 05-ch3/interpreters/LET-3.13/src/Let/AST.hs
@@ -0,0 +1,23 @@
+module Let.AST (Program(..), Expr(..), Number, Id) where
+
+data Program = Program Expr deriving Show
+
+data Expr
+  = Const Number
+  | Var Id
+  | Diff Expr Expr
+  | Minus Expr
+  | Add Expr Expr
+  | Mul Expr Expr
+  | Div Expr Expr
+  | Zero Expr
+  | Equal Expr Expr
+  | Greater Expr Expr
+  | Less Expr Expr
+  | If Expr Expr Expr
+  | Let Id Expr Expr
+  deriving Show
+
+type Number = Integer
+
+type Id = String
