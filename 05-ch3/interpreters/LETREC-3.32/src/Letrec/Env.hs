648c419324cdc9aba394af6e486cdb837961ab17
Dwayne Crooks <me@dwaynecrooks.com>
2020-04-05 11:04:14 -0400

    ex3.32 - Add support for any number of mutually recursive unary procedures

--- 05-ch3/interpreters/LETREC-3.32/src/Letrec/Env.hs
+++ 05-ch3/interpreters/LETREC-3.32/src/Letrec/Env.hs
@@ -1,9 +1,11 @@
 module Letrec.Env (Env, empty, extend, extendRec, apply) where
 
+import qualified Data.List as List
+
 data Env s v e
   = Empty
   | Bind s v (Env s v e) -- name value
-  | RecBind s s e (Env s v e) -- name param body
+  | RecBind [(s, s, e)] (Env s v e) -- [(name, param, body)]
 
 -- s represents the type for identifiers
 -- v represents the type for values
@@ -15,7 +17,7 @@ empty = Empty
 extend :: s -> v -> Env s v e -> Env s v e
 extend = Bind
 
-extendRec :: s -> s -> e -> Env s v e -> Env s v e
+extendRec :: [(s, s, e)] -> Env s v e -> Env s v e
 extendRec = RecBind
 
 apply :: (Eq s, Show s) => Env s v e -> s -> (s -> e -> Env s v e -> v) -> v
@@ -30,10 +32,12 @@ apply env name makeValue =
       else
         apply nextEnv name makeValue
 
-    RecBind procName param body nextEnv ->
-      if name == procName then
-        makeValue param body env
-        -- N.B. We use `env`, the environment in which `procName` is defined.
-        -- This is the key to making `letrec` work.
-      else
-        apply nextEnv name makeValue
+    RecBind recProcs nextEnv ->
+      case List.find (\(procName, _, _) -> name == procName) recProcs of
+        Nothing ->
+          apply nextEnv name makeValue
+
+        Just (_, param, body) ->
+          makeValue param body env
+          -- N.B. We use `env`, the environment in which `procName` is defined.
+          -- This is the key to making `letrec` work.
