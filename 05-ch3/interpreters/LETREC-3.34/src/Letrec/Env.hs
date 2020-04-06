08cc54ff3a81a379da35bbfc59964023b1cf1bc8
Dwayne Crooks <me@dwaynecrooks.com>
2020-04-06 17:04:16 -0400

    ex3.34 - Lay the foundation and start with LETREC

---
+++ 05-ch3/interpreters/LETREC-3.34/src/Letrec/Env.hs
@@ -0,0 +1,39 @@
+module Letrec.Env (Env, empty, extend, extendRec, apply) where
+
+data Env s v e
+  = Empty
+  | Bind s v (Env s v e) -- name value
+  | RecBind s s e (Env s v e) -- name param body
+
+-- s represents the type for identifiers
+-- v represents the type for values
+-- e represents the type for expressions
+
+empty :: Env s v e
+empty = Empty
+
+extend :: s -> v -> Env s v e -> Env s v e
+extend = Bind
+
+extendRec :: s -> s -> e -> Env s v e -> Env s v e
+extendRec = RecBind
+
+apply :: (Eq s, Show s) => Env s v e -> s -> (s -> e -> Env s v e -> v) -> v
+apply env name makeValue =
+  case env of
+    Empty ->
+      error ("No binding for " ++ show name)
+
+    Bind varName value nextEnv ->
+      if name == varName then
+        value
+      else
+        apply nextEnv name makeValue
+
+    RecBind procName param body nextEnv ->
+      if name == procName then
+        makeValue param body env
+        -- N.B. We use `env`, the environment in which `procName` is defined.
+        -- This is the key to making `letrec` work.
+      else
+        apply nextEnv name makeValue
