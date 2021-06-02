af02de8555bf60021202596ab52f8e5419a5c810
Dwayne Crooks <me@dwaynecrooks.com>
2021-06-02 07:45:46 -0400

    Implement CONTINUATION-PASSING

--- ch5/CONTINUATION-PASSING/interpreter.rkt
+++ ch5/CONTINUATION-PASSING/interpreter.rkt
@@ -19,56 +19,99 @@
                     (extend-env
                      'x (num-val 10)
                      (empty-env))))])
-    (value-of-program (parse s) init-env)))
+    (value-of-program (parse s) init-env (end-cont))))
 
-(define (value-of-program prog env)
+;; Program x Env x Cont -> FinalAnswer
+(define (value-of-program prog env cont)
   (cases program prog
-    [a-program (exp) (value-of-exp exp env)]))
+    [a-program (exp) (value-of-exp exp env cont)]))
 
-(define (value-of-exp exp env)
+;; Expression x Env x Cont -> FinalAnswer
+(define (value-of-exp exp env cont)
   (cases expression exp
     [const-exp (n)
-               (num-val n)]
+               (apply-cont cont (num-val n))]
 
     [var-exp (var)
-             (apply-env env var construct-proc-val)]
+             (apply-cont cont (apply-env env var construct-proc-val))]
 
     [diff-exp (exp1 exp2)
-              (let ([val1 (value-of-exp exp1 env)]
-                    [val2 (value-of-exp exp2 env)])
-                (num-val
-                 (- (expval->num val1)
-                    (expval->num val2))))]
+              (value-of-exp exp1 env (diff1-cont exp2 env cont))]
 
     [zero?-exp (exp1)
-               (let ([val1 (value-of-exp exp1 env)])
-                 (if (zero? (expval->num val1))
-                     (bool-val #t)
-                     (bool-val #f)))]
+               (value-of-exp exp1 env (zero1-cont cont))]
 
     [if-exp (exp1 exp2 exp3)
-            (let ([val1 (value-of-exp exp1 env)])
-              (if (expval->bool val1)
-                  (value-of-exp exp2 env)
-                  (value-of-exp exp3 env)))]
+            (value-of-exp exp1 env (if-test-cont exp2 exp3 env cont))]
 
     [let-exp (var exp1 body)
-             (let ([val1 (value-of-exp exp1 env)])
-               (value-of-exp body (extend-env var val1 env)))]
+             (value-of-exp exp1 env (let-exp-cont var body env cont))]
 
     [proc-exp (var body)
-              (proc-val (procedure var body env))]
+              (apply-cont cont (proc-val (procedure var body env)))]
 
     [letrec-exp (proc-name bound-var proc-body letrec-body)
-                (value-of-exp letrec-body (extend-env-rec proc-name bound-var proc-body env))]
+                (value-of-exp
+                 letrec-body
+                 (extend-env-rec proc-name bound-var proc-body env)
+                 cont)]
 
     [call-exp (rator rand)
-              (let ([proc (expval->proc (value-of-exp rator env))]
-                    [arg (value-of-exp rand env)])
-                (apply-procedure proc arg))]))
+              (value-of-exp rator env (rator-cont rand env cont))]))
 
 (define (construct-proc-val var body saved-env)
   (proc-val (procedure var body saved-env)))
+
+;; Continuations
+;;
+;; It uses a procedural representation.
+
+(define (end-cont)
+  (lambda (val)
+    (eopl:printf "End of computation.~%")
+    val))
+
+(define (zero1-cont cont)
+  (lambda (val)
+    ; (apply-cont cont (if (zero? (expval->num val))
+    ;                      (bool-val #t)
+    ;                      (bool-val #f)))
+    ; This can be simplified to:
+    (apply-cont cont (bool-val (zero? (expval->num val))))))
+
+(define (let-exp-cont var body env cont)
+  (lambda (val)
+    (value-of-exp body (extend-env var val env) cont)))
+
+(define (if-test-cont exp2 exp3 env cont)
+  (lambda (val)
+    (if (expval->bool val)
+        (value-of-exp exp2 env cont)
+        (value-of-exp exp3 env cont))))
+
+(define (diff1-cont exp2 env cont)
+  (lambda (val1)
+    (value-of-exp exp2 env (diff2-cont val1 cont))))
+
+(define (diff2-cont val1 cont)
+  (lambda (val2)
+    (apply-cont
+     cont
+     (num-val
+      (- (expval->num val1)
+         (expval->num val2))))))
+
+(define (rator-cont rand env cont)
+  (lambda (proc-val)
+    (value-of-exp rand env (rand-cont proc-val cont))))
+
+(define (rand-cont proc-val cont)
+  (lambda (arg)
+    (apply-procedure (expval->proc proc-val) arg cont)))
+
+;; Cont x ExpVal -> FinalAnswer
+(define (apply-cont cont val)
+  (cont val))
 
 ;; Procedure ADT
 
@@ -78,10 +121,10 @@
    (body expression?)
    (saved-env env?)])
 
-(define (apply-procedure proc1 val)
+(define (apply-procedure proc1 val cont)
   (cases proc proc1
     [procedure (var body saved-env)
-               (value-of-exp body (extend-env var val saved-env))]))
+               (value-of-exp body (extend-env var val saved-env) cont)]))
 
 ;; Values
 ;;
