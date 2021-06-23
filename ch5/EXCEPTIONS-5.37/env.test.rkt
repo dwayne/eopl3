cb9c9e3171a27492309bf631d696a191caac1fef
Dwayne Crooks <me@dwaynecrooks.com>
2021-06-23 06:37:58 -0400

    EXCEPTIONS-5.37 - Start with EXCEPTIONS

---
+++ ch5/EXCEPTIONS-5.37/env.test.rkt
@@ -0,0 +1,21 @@
+#lang racket
+
+(require "./env.rkt")
+
+(require rackunit)
+
+(let ([env (extend-env
+            'd 6
+            (extend-env
+             'y 8
+             (extend-env
+              'x 7
+              (extend-env
+               'y 14
+               (empty-env)))))])
+
+  (check-eq? (apply-env env 'd #f) 6)
+  (check-eq? (apply-env env 'y #f) 8)
+  (check-eq? (apply-env env 'x #f) 7)
+
+  (check-exn #rx"No binding for a" (lambda () (apply-env env 'a #f))))
