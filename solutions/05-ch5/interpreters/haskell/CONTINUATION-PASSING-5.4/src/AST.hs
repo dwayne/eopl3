0ee523c91d3dc9b4ba56cfaebc89b28f3ed50f93
Dwayne Crooks <me@dwaynecrooks.com>
2022-07-25 05:13:37 -0400

    CONTINUATION-PASSING-5.4: Start with CONTINUATION-PASSING-5.3

---
+++ solutions/05-ch5/interpreters/haskell/CONTINUATION-PASSING-5.4/src/AST.hs
@@ -0,0 +1,21 @@
+module AST (Program(..), Expr(..), Number, Id) where
+
+
+data Program = Program Expr deriving (Eq, Show)
+
+data Expr
+  = Const Number
+  | Var Id
+  | Diff Expr Expr
+  | Zero Expr
+  | If Expr Expr Expr
+  | Let Id Expr Expr
+  | Let2 Id Expr Id Expr Expr
+  | Proc Id Expr
+  | Letrec Id Id Expr Expr
+  | Call Expr Expr
+  deriving (Eq, Show)
+
+type Number = Integer
+
+type Id = String
