72745145087920a931a67058a2e052a72485ded2
Dwayne Crooks <me@dwaynecrooks.com>
2022-07-26 04:37:02 -0400

    CONTINUATION-PASSING-5.7: Start with CONTINUATION-PASSING-5.6

---
+++ solutions/05-ch5/interpreters/haskell/CONTINUATION-PASSING-5.7/tests/Test/InterpreterSpec.hs
@@ -0,0 +1,116 @@
+module Test.InterpreterSpec (spec) where
+
+
+import Interpreter
+import Test.Hspec
+
+
+spec :: Spec
+spec =
+  describeExamples "interpreter"
+    [ ( "5", VNumber 5 )
+    , ( "x", VNumber 10 )
+    , ( "zero?(i)", VBool False )
+    , ( "zero?(-(i, 1))", VBool True )
+    , ( "-(55, -(x, 11))", VNumber 56 )
+    , ( "-(-(x, 3), -(v, i))", VNumber 3 )
+    , ( "let x = 33                                      \
+        \in let y = 22                                   \
+        \in if zero?(-(x, 11)) then -(y, 2) else -(y, 4) "
+      , VNumber 18
+      )
+    , ( "let x = 5 in -(x, 3)", VNumber 2 )
+    , ( "let z = 5                           \
+        \in let x = 3                        \
+        \   in let y = -(x, 1)               \
+        \      in let x = 4 in -(z, -(x, y)) "
+      , VNumber 3
+      )
+    , ( "let x = 7                     \
+        \in let y = 2                  \
+        \   in let y = let x = -(x, 1) \
+        \              in -(x, y)      \
+        \      in -(-(x, 8), y)        "
+      , VNumber (-5)
+      )
+    , ( "let f = proc (x) -(x, 11) \
+        \in (f (f 77))             "
+      , VNumber 55
+      )
+    , ( "(proc (f) (f (f 77)) \
+        \ proc (x) -(x, 11))  "
+      , VNumber 55
+      )
+    , ( "let x = 200                       \
+        \in let f = proc (z) -(z, x)       \
+        \   in let x = 100                 \
+        \      in let g = proc (z) -(z, x) \
+        \         in -((f 1), (g 1))       "
+      , VNumber (-100)
+      )
+
+    -- Exercise 3.20
+    --
+    -- Write a curried procedure that takes two arguments and returns their sum.
+    , ( "let f = proc (x) proc (y) -(x, -(0, y)) \
+        \in let a = ((f 1) 2)                    \
+        \   in let b = ((f a) 3)                 \
+        \      in let c = ((f b) 4)              \
+        \         in let d = ((f c) 5) in d      "
+      , VNumber 15
+      )
+
+    , ( "letrec double(x) = if zero?(x)                       \
+        \                   then 0                            \
+        \                   else -((double -(x, 1)), -(0, 2)) \
+        \in (double 6)                                        "
+      , VNumber 12
+      )
+
+    -- Exercise 5.3
+    , ( "let2                    \
+        \  x = 33                \
+        \  y = 22                \
+        \in                      \
+        \if zero?(-(x, 11)) then \
+        \  -(y, 2)               \
+        \else                    \
+        \  -(y, 4)               "
+      , VNumber 18
+      )
+
+    -- Exercise 5.4
+    , ( "let3         \
+        \  a = 10     \
+        \  b = 5      \
+        \  c = 1      \
+        \in           \
+        \-(a, -(b, c))"
+      , VNumber 6
+      )
+
+    -- Exercise 5.5
+    , ( "emptylist", VList [] )
+    , ( "cons(1, emptylist)", VList [ VNumber 1 ] )
+    , ( "cons(1, cons(2, emptylist))", VList [ VNumber 1, VNumber 2] )
+    , ( "car(cons (1, emptylist))", VNumber 1 )
+    , ( "cdr(cons(1, cons(2, emptylist)))", VList [ VNumber 2 ] )
+    , ( "null?(emptylist)", VBool True )
+    , ( "null?(cons(1, emptylist))", VBool False )
+
+    -- Exercise 5.6
+    , ( "list()", VList [] )
+    , ( "list(1)", VList [ VNumber 1] )
+    , ( "list(1, 2)", VList [ VNumber 1, VNumber 2 ] )
+    , ( "list(-(2, 1), -(4, 2))", VList [ VNumber 1, VNumber 2 ] )
+    ]
+
+
+describeExamples :: String -> [(String, Value)] -> Spec
+describeExamples description =
+  describe description . mapM_ (uncurry toSpecItem) . zip [1..]
+  where
+    toSpecItem :: Int -> (String, Value) -> Spec
+    toSpecItem i (input, value) =
+      it ("example " ++ show i) $ do
+        run input `shouldBe` Right value
---
+++ solutions/05-ch5/interpreters/haskell/CONTINUATION-PASSING-5.7/tests/Test/ParserSpec.hs
@@ -0,0 +1,54 @@
+module Test.ParserSpec (spec) where
+
+
+import Parser
+import Test.Hspec
+
+
+spec :: Spec
+spec =
+  describeExamples "parser"
+    [ ( "1", Const 1 )
+    , ( "x", Var "x" )
+    , ( "-(5, y)", Diff (Const 5) (Var "y") )
+    , ( "zero?(z)", Zero (Var "z") )
+    , ( "if zero?(2) then 0 else 1", If (Zero (Const 2)) (Const 0) (Const 1) )
+    , ( "let n = 10 in -(n, 1)", Let "n" (Const 10) (Diff (Var "n") (Const 1)) )
+    , ( "proc (x) -(x, 1)", Proc "x" (Diff (Var "x") (Const 1)) )
+    , ( "letrec f(x) = a in b", Letrec "f" "x" (Var "a") (Var "b") )
+    , ( "(f x)", Call (Var "f") (Var "x") )
+
+    , ( "let2 a = 1 b = 2 in -(b, a)"
+      , Let2 "a" (Const 1) "b" (Const 2) (Diff (Var "b") (Var "a"))
+      )
+
+    , ( "let3 a = 1 b = 2 c = 3 in -(c, -(b, a))"
+      , Let3
+          "a" (Const 1)
+          "b" (Const 2)
+          "c" (Const 3)
+          (Diff (Var "c") (Diff (Var "b") (Var "a")))
+      )
+
+    -- Exercise 5.5
+    , ( "cons(1, emptylist)", Cons (Const 1) EmptyList )
+    , ( "car(l)", Car (Var "l") )
+    , ( "cdr(l)", Cdr (Var "l") )
+    , ( "null?(emptylist)", Null EmptyList )
+    , ( "emptylist", EmptyList )
+
+    -- Exercise 5.6
+    , ( "list()", List [] )
+    , ( "list(1)", List [Const 1] )
+    , ( "list(1, 2)", List [Const 1, Const 2] )
+    ]
+
+
+describeExamples :: String -> [(String, Expr)] -> Spec
+describeExamples description =
+  describe description . mapM_ (uncurry toSpecItem) . zip [1..]
+  where
+    toSpecItem :: Int -> (String, Expr) -> Spec
+    toSpecItem i (input, expr) =
+      it ("example " ++ show i) $ do
+        parse input `shouldBe` Right (Program expr)
