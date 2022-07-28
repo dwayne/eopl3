5cca1dc257fa5f94bf5bf1d89acfbfef55ef912c
Dwayne Crooks <me@dwaynecrooks.com>
2022-07-28 06:46:33 -0400

    CONTINUATION-PASSING-5.9: Add the store to the interpreter

--- solutions/05-ch5/interpreters/haskell/CONTINUATION-PASSING-5.9/src/Interpreter.hs
+++ solutions/05-ch5/interpreters/haskell/CONTINUATION-PASSING-5.9/src/Interpreter.hs
@@ -7,14 +7,16 @@ module Interpreter
 
 
 import qualified Env
+import qualified Store
 
-import Data.Bifunctor (first)
+import Data.Bifunctor (bimap)
 import Parser
 
 
 data Value
   = VNumber Number
   | VBool Bool
+  | VRef Store.Ref
   | VProc Procedure
 
 data Procedure
@@ -23,10 +25,13 @@ data Procedure
 data Type
   = TNumber
   | TBool
+  | TRef
   | TProc
   deriving (Eq, Show)
 
 type Env = Env.Env Id Value Id Expr
+
+type Store = Store.Store Value
 
 data Error
   = SyntaxError ParseError
@@ -42,12 +47,14 @@ data RuntimeError
 instance Eq Value where
   (VNumber n1) == (VNumber n2) = n1 == n2
   (VBool b1) == (VBool b2) = b1 == b2
+  (VRef r1) == (VRef r2) = r1 == r2
   _ == _ = False
 
 
 instance Show Value where
   show (VNumber n) = show n
   show (VBool b) = show b
+  show (VRef r) = show r
   show (VProc _) = "<proc>"
 
 
@@ -58,12 +65,12 @@ run input =
       Left $ SyntaxError err
 
     Right program ->
-      first RuntimeError $ valueOfProgram program
+      bimap RuntimeError fst $ valueOfProgram program
 
 
-valueOfProgram :: Program -> Either RuntimeError Value
+valueOfProgram :: Program -> Either RuntimeError (Value, Store)
 valueOfProgram (Program expr) =
-  valueOfExpr expr initEnv
+  valueOfExpr expr initEnv initStore
   where
     initEnv =
       Env.extend "i" (VNumber 1)
@@ -71,77 +78,80 @@ valueOfProgram (Program expr) =
           (Env.extend "x" (VNumber 10)
             Env.empty))
 
+    initStore =
+      Store.empty
 
-valueOfExpr :: Expr -> Env -> Either RuntimeError Value
-valueOfExpr expr env =
+
+valueOfExpr :: Expr -> Env -> Store -> Either RuntimeError (Value, Store)
+valueOfExpr expr env store =
   case expr of
     Const n ->
-      Right $ VNumber n
+      Right (VNumber n, store)
 
     Var x ->
       case Env.find x env of
         Just (Env.Value value) ->
-          Right value
+          Right (value, store)
 
         Just (Env.Procedure param body savedEnv) ->
-          Right $ VProc $ Procedure param body savedEnv
+          Right (VProc $ Procedure param body savedEnv, store)
 
         Nothing ->
           Left $ IdentifierNotFound x
 
     Diff aExpr bExpr -> do
-      aValue <- valueOfExpr aExpr env
-      bValue <- valueOfExpr bExpr env
-      diff aValue bValue
+      (aValue, store1) <- valueOfExpr aExpr env store
+      (bValue, store2) <- valueOfExpr bExpr env store1
+      diff aValue bValue store2
 
     Zero aExpr -> do
-      aValue <- valueOfExpr aExpr env
-      zero aValue
+      (aValue, store1) <- valueOfExpr aExpr env store
+      zero aValue store1
 
     If condition consequent alternative -> do
-      conditionValue <- valueOfExpr condition env
-      computeIf conditionValue consequent alternative env
+      (conditionValue, store1) <- valueOfExpr condition env store
+      computeIf conditionValue consequent alternative env store1
 
     Let x aExpr body -> do
-      aValue <- valueOfExpr aExpr env
-      valueOfExpr body $ Env.extend x aValue env
+      (aValue, store1) <- valueOfExpr aExpr env store
+      valueOfExpr body (Env.extend x aValue env) store1
 
     Proc param body ->
-      return $ VProc $ Procedure param body env
+      Right (VProc $ Procedure param body env, store)
 
     Letrec declarations letrecBody ->
-      valueOfExpr letrecBody $ Env.extendRec declarations env
+      valueOfExpr letrecBody (Env.extendRec declarations env) store
 
     Call rator rand -> do
-      ratorValue <- valueOfExpr rator env
-      randValue <- valueOfExpr rand env
-      apply ratorValue randValue
+      (ratorValue, store1) <- valueOfExpr rator env store
+      (randValue, store2) <- valueOfExpr rand env store1
+      apply ratorValue randValue store2
 
 
-diff :: Value -> Value -> Either RuntimeError Value
-diff aValue bValue = do
+diff :: Value -> Value -> Store -> Either RuntimeError (Value, Store)
+diff aValue bValue store = do
   a <- toNumber aValue
   b <- toNumber bValue
-  return $ VNumber $ a - b
+  Right (VNumber $ a - b, store)
 
 
-zero :: Value -> Either RuntimeError Value
-zero aValue = do
+zero :: Value -> Store -> Either RuntimeError (Value, Store)
+zero aValue store = do
   a <- toNumber aValue
-  return $ VBool $ a == 0
+  Right (VBool $ a == 0, store)
 
 
-computeIf :: Value -> Expr -> Expr -> Env -> Either RuntimeError Value
-computeIf conditionValue consequent alternative env = do
+computeIf :: Value -> Expr -> Expr -> Env -> Store -> Either RuntimeError (Value, Store)
+computeIf conditionValue consequent alternative env store = do
   a <- toBool conditionValue
   let expr = if a then consequent else alternative
-  valueOfExpr expr env
+  valueOfExpr expr env store
 
 
-apply :: Value -> Value -> Either RuntimeError Value
-apply ratorValue arg = do
+apply :: Value -> Value -> Store -> Either RuntimeError (Value, Store)
+apply ratorValue arg store = do
   Procedure param body savedEnv <- toProcedure ratorValue
-  valueOfExpr body $ Env.extend param arg savedEnv
+  valueOfExpr body (Env.extend param arg savedEnv) store
 
 
 toNumber :: Value -> Either RuntimeError Number
@@ -162,4 +172,5 @@ toProcedure value = Left $ TypeError TProc (typeOf value)
 typeOf :: Value -> Type
 typeOf (VNumber _) = TNumber
 typeOf (VBool _) = TBool
+typeOf (VRef _) = TRef
 typeOf (VProc _) = TProc
