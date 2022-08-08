1a27247cb0b8de44c6d54e8f9243fc264c86f659
Dwayne Crooks <me@dwaynecrooks.com>
2022-08-08 08:00:31 -0400

    EXCEPTIONS-5.37: Start with EXCEPTIONS

---
+++ solutions/05-ch5/interpreters/haskell/EXCEPTIONS-5.37/tests/Test/ParserSpec.hs
@@ -0,0 +1,32 @@
+module Test.ParserSpec (spec) where
+
+
+import Parser
+import Test.Hspec
+
+
+spec :: Spec
+spec =
+  describeExamples "parser"
+    [ ( "1", Const 1 )
+    , ( "x", Var "x" )
+    , ( "-(5, y)", Diff (Const 5) (Var "y") )
+    , ( "zero?(z)", Zero (Var "z") )
+    , ( "if zero?(2) then 0 else 1", If (Zero (Const 2)) (Const 0) (Const 1) )
+    , ( "let n = 10 in -(n, 1)", Let "n" (Const 10) (Diff (Var "n") (Const 1)) )
+    , ( "proc (x) -(x, 1)", Proc "x" (Diff (Var "x") (Const 1)) )
+    , ( "letrec f(x) = a in b", Letrec "f" "x" (Var "a") (Var "b") )
+    , ( "(f x)", Call (Var "f") (Var "x") )
+    , ( "try 1 catch (x) 2", Try (Const 1) "x" (Const 2) )
+    , ( "raise 99", Raise (Const 99) )
+    ]
+
+
+describeExamples :: String -> [(String, Expr)] -> Spec
+describeExamples description =
+  describe description . mapM_ (uncurry toSpecItem) . zip [1..]
+  where
+    toSpecItem :: Int -> (String, Expr) -> Spec
+    toSpecItem i (input, expr) =
+      it ("example " ++ show i) $ do
+        parse input `shouldBe` Right (Program expr)
