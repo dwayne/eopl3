e1c85e88fcaa45deb29f24d9525af70e2bfb6638
Dwayne Crooks <me@dwaynecrooks.com>
2022-08-22 07:15:07 -0400

    THREADS: Start with a slightly modified version of CONTINUATION-PASSING-5.9

---
+++ solutions/05-ch5/interpreters/haskell/THREADS/src/Parser.hs
@@ -0,0 +1,98 @@
+module Parser
+  ( module AST
+  , parse
+  , ParseError
+  )
+  where
+
+
+import qualified Text.Parsec as P
+
+import AST
+import Lexer
+import Text.Parsec ((<|>), ParseError)
+import Text.Parsec.String (Parser)
+
+
+parse :: String -> Either ParseError Program
+parse =
+  P.parse program ""
+
+
+program :: Parser Program
+program =
+  Program <$ whiteSpace <*> expr <* P.eof
+
+
+expr :: Parser Expr
+expr
+  = constExpr
+  <|> varExpr
+  <|> diffExpr
+  <|> zeroExpr
+  <|> ifExpr
+  <|> letExpr
+  <|> procExpr
+  <|> letrecExpr
+  <|> callExpr
+  <|> beginExpr
+  <|> assignExpr
+
+
+constExpr :: Parser Expr
+constExpr =
+  Const <$> number
+
+
+varExpr :: Parser Expr
+varExpr =
+  Var <$> identifier
+
+
+diffExpr :: Parser Expr
+diffExpr =
+  hyphen *> parens (Diff <$> expr <*> (comma *> expr))
+
+
+zeroExpr :: Parser Expr
+zeroExpr =
+  Zero <$ rZero <*> parens expr
+
+
+ifExpr :: Parser Expr
+ifExpr =
+  If <$ rIf <*> expr <*> (rThen *> expr) <*> (rElse *> expr)
+
+
+letExpr :: Parser Expr
+letExpr =
+  Let <$ rLet <*> identifier <*> (equal *> expr) <*> (rIn *> expr)
+
+
+procExpr :: Parser Expr
+procExpr =
+  Proc <$ rProc <*> parens identifier <*> expr
+
+
+letrecExpr :: Parser Expr
+letrecExpr =
+  let
+    declaration =
+      (,,) <$> identifier <*> parens identifier <*> (equal *> expr)
+  in
+  Letrec <$ rLetrec <*> P.many declaration <*> (rIn *> expr)
+
+
+callExpr :: Parser Expr
+callExpr =
+  parens (Call <$> expr <*> expr)
+
+
+beginExpr :: Parser Expr
+beginExpr =
+  Begin <$ rBegin <*> semiSep expr <* rEnd
+
+
+assignExpr :: Parser Expr
+assignExpr =
+  Assign <$ rSet <*> identifier <*> (equal *> expr)
---
+++ solutions/05-ch5/interpreters/haskell/THREADS/tests/Test/ParserSpec.hs
@@ -0,0 +1,44 @@
+module Test.ParserSpec (spec) where
+
+
+import Parser
+import Test.Hspec
+
+
+spec :: Spec
+spec =
+  describeExamples "parser"
+    [ ( "1", Const 1 )
+    , ( "x", Var "x" )
+    , ( "-(5, y)", Diff (Const 5) (Var "y") )
+    , ( "zero?(z)", Zero (Var "z") )
+    , ( "if zero?(2) then 0 else 1", If (Zero (Const 2)) (Const 0) (Const 1) )
+    , ( "let n = 10 in -(n, 1)", Let "n" (Const 10) (Diff (Var "n") (Const 1)) )
+    , ( "proc (x) -(x, 1)", Proc "x" (Diff (Var "x") (Const 1)) )
+    , ( "letrec f(x) = a in b"
+      , Letrec [("f", "x", (Var "a"))] (Var "b")
+      )
+    , ( "(f x)", Call (Var "f") (Var "x") )
+    , ( "begin    \
+        \  1;     \
+        \  x;     \
+        \ -(5, y) \
+        \end      "
+      , Begin
+          [ Const 1
+          , Var "x"
+          , Diff (Const 5) (Var "y")
+          ]
+      )
+    , ( "set x = 1", Assign "x" (Const 1) )
+    ]
+
+
+describeExamples :: String -> [(String, Expr)] -> Spec
+describeExamples description =
+  describe description . mapM_ (uncurry toSpecItem) . zip [1..]
+  where
+    toSpecItem :: Int -> (String, Expr) -> Spec
+    toSpecItem i (input, expr) =
+      it ("example " ++ show i) $ do
+        parse input `shouldBe` Right (Program expr)
