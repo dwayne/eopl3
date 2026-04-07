4400816775421498e1556b0a220ce1191997138e
Dwayne Crooks <me@dwaynecrooks.com>
2026-04-07 06:14:40 -0400

    Update solutions/06-ch6/factorial
      - Add a README
      - Remove Setup.hs, stack.yaml, and stack.yaml.lock
      - Refactor factorial.cabal
      - Add a test

---
+++ solutions/06-ch6/factorial/README.md
@@ -0,0 +1,10 @@
+# A continuation-passing version of factorial
+
+```bash
+cabal repl
+ghci> Factorial.fact 5
+ghci> Factorial.Iterative.fact 5
+ghci> Factorial.CPSDataStructure.fact 5
+ghci> Factorial.CPSProcedural.fact 5
+ghci> Factorial.Registerized.fact 5
+```
---
+++ solutions/06-ch6/factorial/test/Test/FactorialSpec.hs
@@ -0,0 +1,20 @@
+module Test.FactorialSpec (spec) where
+
+import qualified Factorial
+import qualified Factorial.Iterative
+import qualified Factorial.CPSDataStructure
+import qualified Factorial.CPSProcedural
+import qualified Factorial.Registerized
+
+import Test.Hspec
+
+
+spec :: Spec
+spec =
+    describe "fact" $ do
+        it "computes 5!" $ do
+            Factorial.fact 5 `shouldBe` 120
+            Factorial.Iterative.fact 5 `shouldBe` 120
+            Factorial.CPSDataStructure.fact 5 `shouldBe` 120
+            Factorial.CPSProcedural.fact 5 `shouldBe` 120
+            Factorial.Registerized.fact 5 `shouldBe` 120
