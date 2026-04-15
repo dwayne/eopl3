017e33ae06b345fab48bdbabebd096aeda15f15d
Dwayne Crooks <me@dwaynecrooks.com>
2026-04-14 20:46:27 -0400

    Exercise 6.13: Test occurs-in?

--- solutions/06-ch6/racket/ex6.13.rkt
+++ solutions/06-ch6/racket/ex6.13.rkt
@@ -1,6 +1,6 @@
 #lang racket
 
-;; 1.
+;; 1
 (define (removeall n s k)
   (if (null? s)
       (k '())
@@ -19,9 +19,32 @@
 (module+ test
   (require rackunit)
 
+  (define (endk x)
+    (displayln "End of computation")
+    x)
+
   (check-equal?
-   (removeall 1 '(1 2 3 4 1 2 1)
-              (lambda (n)
-                (displayln "End of computation")
-                n))
+   (removeall 1 '(1 2 3 4 1 2 1) endk)
    '(2 3 4 2)))
+
+;; 2
+(define (occurs-in? n s k)
+  (if (null? s)
+      (k #f)
+      (if (number? (car s))
+          (if (equal? n (car s))
+              (k #t)
+              (occurs-in? n (cdr s) k))
+          (occurs-in? n (car s)
+                      (lambda (b)
+                        (if b
+                            (k #t)
+                            (occurs-in? n (cdr s) k)))))))
+
+(module+ test
+  (check-equal?
+   (occurs-in? 5 '((1 2) 3 (5 4)) endk)
+   #t)
+  (check-equal?
+   (occurs-in? 6 '((1 2) 3 (5 4)) endk)
+   #f))
