e2981e6648bc354628f31ce1c7764a138c265580
Dwayne Crooks <me@dwaynecrooks.com>
2026-04-28 06:52:11 -0400

    CPS-TRANSFORMER: My first attempt at cpsOfExprs

--- solutions/06-ch6/interpreters/haskell/CPS-TRANSFORMER/src/Transformer.hs
+++ solutions/06-ch6/interpreters/haskell/CPS-TRANSFORMER/src/Transformer.hs
@@ -39,3 +39,109 @@ cpsOfExpr expr k =
 
         _ ->
             error "To be implemented"
+
+
+cpsOfExprs :: [CPS_IN_AST.Expr] -> ([CPS_OUT_AST.SimpleExpr] -> CPS_OUT_AST.TfExpr) -> CPS_OUT_AST.TfExpr
+cpsOfExprs exprs build =
+    case findFirstNonSimpleExpr exprs of
+        Nothing ->
+            --
+            -- All are simple.
+            --
+            build (map toSimpleExpr exprs)
+
+        Just (before, expr, after) ->
+            --
+            -- before is a list of simple expressions
+            -- expr is a non-simple expression
+            -- after is a list of simple/non-simple expressions
+            --
+            let
+                --
+                -- N.B. This isn't quite right. vid needs to be unique.
+                --
+                vid = "_v"
+                vvar = CPS_IN_AST.Var vid
+            in
+            cpsOfExpr expr (CPS_OUT_AST.Proc [vid] (cpsOfExprs (before ++ [vvar] ++ after) build))
+
+
+
+isSimpleExpr :: CPS_IN_AST.Expr -> Bool
+isSimpleExpr expr =
+    case expr of
+        CPS_IN_AST.Const _ ->
+            True
+
+        CPS_IN_AST.Var _ ->
+            True
+
+        CPS_IN_AST.Diff a b ->
+            isSimpleExpr a && isSimpleExpr b
+
+        CPS_IN_AST.Zero e ->
+            isSimpleExpr e
+
+        CPS_IN_AST.Proc _ _ ->
+            True
+
+        _ ->
+            False
+
+
+toSimpleExpr :: CPS_IN_AST.Expr -> CPS_OUT_AST.SimpleExpr
+toSimpleExpr expr =
+    --
+    -- N.B. Assumes isSimpleExpr expr is true.
+    --
+    case expr of
+        CPS_IN_AST.Const n ->
+            CPS_OUT_AST.Const n
+
+        CPS_IN_AST.Var v ->
+            CPS_OUT_AST.Var v
+
+        CPS_IN_AST.Diff a b ->
+            CPS_OUT_AST.Diff (toSimpleExpr a) (toSimpleExpr b)
+
+        CPS_IN_AST.Zero e ->
+            CPS_OUT_AST.Zero (toSimpleExpr e)
+
+        CPS_IN_AST.Proc vars body ->
+            let
+                --
+                -- See previous note about kid.
+                --
+                kid = "_k"
+                kvar = CPS_OUT_AST.Var kid
+            in
+            CPS_OUT_AST.Proc (vars ++ [kid]) (cpsOfExpr body kvar)
+
+        _ ->
+            --
+            -- If the algorithm is correct then this should never happen.
+            --
+            error "Logical error"
+
+
+findFirstNonSimpleExpr :: [CPS_IN_AST.Expr] -> Maybe ([CPS_IN_AST.Expr], CPS_IN_AST.Expr, [CPS_IN_AST.Expr])
+findFirstNonSimpleExpr =
+    findFirstNonSimpleExprHelper []
+
+
+findFirstNonSimpleExprHelper :: [CPS_IN_AST.Expr] -> [CPS_IN_AST.Expr] -> Maybe ([CPS_IN_AST.Expr], CPS_IN_AST.Expr, [CPS_IN_AST.Expr])
+findFirstNonSimpleExprHelper verp exprs =
+    case exprs of
+        [] ->
+            Nothing
+
+        expr : restOfExprs ->
+            if isSimpleExpr expr then
+                findFirstNonSimpleExprHelper (expr : verp) restOfExprs
+
+            else
+                Just
+                    ( reverse verp
+                    , expr
+                    , restOfExprs
+                    )
